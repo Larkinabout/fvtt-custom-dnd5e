6bb6993e94bbd07bc0162d5966f39f0d730f8b75
Russell Larkin <russell.larkin@gmail.com>
2026-09-18 08:09:26 +0100

    Fix custom character sheet

--- scripts/sheets/character-sheet.js
+++ scripts/sheets/character-sheet.js
@@ -22,70 +22,18 @@ export class CustomDnd5eCharacterActorSheet extends dnd5e.applications.actor.Cha
   /* -------------------------------------------- */
 
   static PARTS = {
-    header: {
-      template: "systems/dnd5e/templates/actors/character-header.hbs"
-    },
-    sidebar: {
-      container: { classes: ["main-content"], id: "main" },
-      template: "systems/dnd5e/templates/actors/character-sidebar.hbs"
-    },
+    ...super.PARTS,
     details: {
       container: { classes: ["tab-body"], id: "tabs" },
       template: "modules/custom-dnd5e/templates/sheet/character-details.hbs",
       templates: ["systems/dnd5e/templates/actors/character-ability-scores.hbs"],
       scrollable: [""]
-    },
-    inventory: {
-      container: { classes: ["tab-body"], id: "tabs" },
-      template: "systems/dnd5e/templates/actors/tabs/character-inventory.hbs",
-      templates: [
-        "systems/dnd5e/templates/inventory/inventory.hbs", "systems/dnd5e/templates/inventory/activity.hbs",
-        "systems/dnd5e/templates/inventory/encumbrance.hbs", "systems/dnd5e/templates/inventory/containers.hbs"
-      ],
-      scrollable: [""]
-    },
-    features: {
-      container: { classes: ["tab-body"], id: "tabs" },
-      template: "systems/dnd5e/templates/actors/tabs/character-features.hbs",
-      templates: ["systems/dnd5e/templates/inventory/inventory.hbs", "systems/dnd5e/templates/inventory/activity.hbs"],
-      scrollable: [""]
-    },
-    spells: {
-      container: { classes: ["tab-body"], id: "tabs" },
-      template: "systems/dnd5e/templates/actors/tabs/creature-spells.hbs",
-      templates: ["systems/dnd5e/templates/inventory/inventory.hbs", "systems/dnd5e/templates/inventory/activity.hbs"],
-      scrollable: [""]
-    },
-    effects: {
-      container: { classes: ["tab-body"], id: "tabs" },
-      template: "systems/dnd5e/templates/actors/tabs/actor-effects.hbs",
-      scrollable: [""]
-    },
-    biography: {
-      container: { classes: ["tab-body"], id: "tabs" },
-      template: "systems/dnd5e/templates/actors/tabs/character-biography.hbs",
-      scrollable: [""]
-    },
-    bastion: {
-      container: { classes: ["tab-body"], id: "tabs" },
-      template: "systems/dnd5e/templates/actors/tabs/character-bastion.hbs",
-      scrollable: [""]
-    },
-    specialTraits: {
-      classes: ["flexcol"],
-      container: { classes: ["tab-body"], id: "tabs" },
-      template: "systems/dnd5e/templates/actors/tabs/creature-special-traits.hbs",
-      scrollable: [""]
-    },
-    warnings: {
-      template: "systems/dnd5e/templates/actors/parts/actor-warnings-dialog.hbs"
-    },
-    tabs: {
-      id: "tabs",
-      classes: ["tabs-right"],
-      template: "systems/dnd5e/templates/shared/sidebar-tabs.hbs"
     }
   };
+
+  static {
+    delete this.PARTS.abilityScores;
+  }
 
   async _preparePartContext(partId, context, options) {
     context = await super._preparePartContext(partId, context, options);
